f6e0d50b9882713c469e20ee2b2b1ba87950a3fd
Venkat53253 <venkatratna532@gmail.com>
2025-08-30 19:23:27 +0530

    hello worlds created

---
+++ 30-vpn/main.tf
@@ -0,0 +1,40 @@
+resource "aws_key_pair" "openvpn" {
+  key_name   = "openvpn"
+  public_key = file("C:\\devops\\veaws.pub") # for mac use /
+}
+
+resource "aws_instance" "vpn" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.vpn_sg_id]
+  subnet_id = local.public_subnet_id
+  #key_name = "veaws" # make sure this key exist in AWS
+  key_name = dataaws_key_pair.veaws.key_name
+  user_data = file("openvpn.sh")
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-vpn"
+    }
+  )
+}
+
+resource "aws_route53_record" "vpn" {
+  zone_id = var.zone_id
+  name    = "vpn-${var.environment}.${var.zone_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.vpn.public_ip]
+  allow_overwrite = true
+}
+
+variable "zone_id" {
+  description = "The Route53 Hosted Zone ID"
+  type        = string
+}
+
+variable "zone_name" {
+  description = "The Route53 zone domain name"
+  type        = string
+}
