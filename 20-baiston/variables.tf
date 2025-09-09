bde659bfb446b25471137a9458db478723f5a2d1
Venkat53253 <venkatratna532@gmail.com>
2025-09-09 12:42:42 +0530

    hello worlds created

--- 20-baiston/variables.tf
+++ 20-baiston/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z05167558BEIFU213OL8"
+}
+
+variable "zone_name" {
+  default = "venaws.site"
+}
